dfe902c147458c1cf443be4203c6a9b51a053c18
Kelian de-Fournier-de-Brescia <kelian.de-fournier-de-brescia@epita.fr>
2022-12-02 00:31:39 +0100

    Cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardMemoComponent } from './card-memo/card-memo.component';
+import { CardAssociateComponent } from './card-associate/card-associate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardMemoComponent,
+    CardAssociateComponent
   ],
   imports: [
     BrowserModule
