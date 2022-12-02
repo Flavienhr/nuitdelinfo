cd60d2804f132309da0b0f91cac47ceac98efca9
Flavien <47445448+Flavienhr@users.noreply.github.com>
2022-12-02 01:56:28 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AssociateComponent } from './associate/associate.component';
 import { CardComponent } from './card/card.component';
 import { CardMemoComponent } from './card-memo/card-memo.component';
 import { CardAssociateComponent } from './card-associate/card-associate.component';
@@ -9,6 +10,7 @@ import { CardAssociateComponent } from './card-associate/card-associate.componen
 @NgModule({
   declarations: [
     AppComponent,
+    AssociateComponent,
     CardComponent,
     CardMemoComponent,
     CardAssociateComponent
