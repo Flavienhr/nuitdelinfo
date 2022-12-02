6bca108da6ef3eb3c24d460decfc089d51779d93
Flavien <flavien_hr@outlook.fr>
2022-12-02 06:21:25 +0100

    init des scores du navbar 🚀

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  percent_text = 0;
-  perc_text = 70;
+  percent_text = "0%";
+  perc_text = 0;
+
+  setPerc(perc:number) {
+    this.perc_text = perc
+    this.percent_text = `${perc}%`
+  }
 }
