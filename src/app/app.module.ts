0da9a3ffc0c5f901a16cbe5b453197dd4962e080
Valentin Oison <valentin.oison@epita.fr>
2022-12-02 02:47:28 +0100

    Quiz 1.0 CSS à faire Base de données des questions à faire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DialogueGameComponent } from './dialogue-game/dialogue-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogueGameComponent
   ],
   imports: [
     BrowserModule
